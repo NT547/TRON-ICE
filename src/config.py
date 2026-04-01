962ef3cfad33f4c34c5fcf0e3618ef7a31893f1f
huynhbao29 <huynhgiabao@gmail.com>
2026-04-01 23:25:07 +0700

    add file craw_tron

---
+++ src/config.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TRONGRID_API = "https://api.trongrid.io/v1"
+TRONGRID_API_KEY = os.getenv("TRONGRID_API_KEY")
+ETHERSCAN_API_KEYS = {
+    "ethereum": os.getenv("ETHERSCAN_API_KEY"),
+    "bsc": os.getenv("BSCSCAN_API_KEY"),
+    "polygon": os.getenv("POLYGONSCAN_API_KEY"),
+}
+
+HOT_WALLETS = {
+    "fixedfloat": ["TDoXUNZ6PajKuiUkcYg3EDSV9bnqGqsbcf"],
+    "changenow": ["TWS1onJnNTg8tJHomceqxBxTsUB1DHh7PV"],
+    "sideshift": ["TBEkD71bkL7fNz2HuCeisRbtvwwT2JWaMr"],
+}
+
+TIME_WINDOW = 600
+VALUE_THRESHOLD = 0.15
+REQUEST_DELAY = 1
+TARGET_TOKENS = ["USDT", "USDC", "TRX"]
