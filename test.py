16452583c5351c90e8c81262dfed700830b13c75
HuynhKhoi2711 <huynhanhkhoi9a7@gmail.com>
2026-04-08 18:09:45 +0700

    updating exacting to samples and csv module

--- test.py
+++ test.py
@@ -2,4 +2,5 @@ from src.preprocessing.exact_to_csv import exact_csv_by_service
 from src.preprocessing.get_samples import exacting_samples
     
 if __name__ == "__main__":
+    # exacting_samples()s
     exact_csv_by_service(data_dir="data/samples")
