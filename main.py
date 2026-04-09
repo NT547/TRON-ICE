c899304db2c9e5cfd4b74969934e86f96f5adbc7
HuynhKhoi2711 <huynhanhkhoi9a7@gmail.com>
2026-04-09 18:59:11 +0700

    fixing encoding error for matching algorithm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from src.identify_operations import classify
+from src.baseline_algorithm import matching
 
 if __name__ == "__main__":
-    classify()
+    # classify()
+    matching(input_dir="data/classified", output_dir="data/matched")
