95da923b8738304d656c67e96a2eb4df6735291d
HuynhKhoi2711 <huynhanhkhoi9a7@gmail.com>
2026-04-11 17:33:51 +0700

    adding labeling module for xgboost

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.utils.labeling import labeling_positive, labeling_negative
+from src.xgboost.labeling import labeling_positive, labeling_negative
 from src.heuristic.baseline_algorithm import matching
 if __name__ == "__main__":
     # labeling_positive()
